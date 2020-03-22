3a61c7a35a0faaa88c95cc3bebbfd6fe68344ef4
Manu Mehta <mmehta27@illinois.edu>
2020-03-22 16:05:25 -0500

    Added some comments.

--- RedditSuicideWatchWebsite.js
+++ RedditSuicideWatchWebsite.js
@@ -5,6 +5,7 @@ function correctInput() {
 	var firstR = input.indexOf("r");
 	var secondR = input.indexOf("r", firstR + 1); 
 	var checkOldRedditCom = input.substring(input.indexOf("o"), secondR + 1);
+	//Checks whether correct link was given.
 	if (checkOldRedditCom == "old.reddit.com/r") {
 		document.getElementById("submitButton").href = "results.html";
 		test();
@@ -15,15 +16,19 @@ function correctInput() {
 }
 
 function invalidInput() {
+	//In the future we can try to make it show alert text so it doesn't need to
+	//redirect to a new link.
 	alert("Invalid Input!");
 }
 
 function submitKeyClicked(keyPressed) {
+	//Allows user to use enter key to submit text value.
 	if (keyPressed.keyCode === 13) {
 		document.getElementById("submitButton").click();
 	}
 }
 
 function test() {
+	//Can remove later, meant to check all the logic was right
 	alert(input + " It worked!");
 }
