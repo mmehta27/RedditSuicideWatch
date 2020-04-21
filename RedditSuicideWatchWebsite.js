80ebe1e243cd8df1aa07a9a3623c6a5ccb0495d7
cook-mary <59888379+cook-mary@users.noreply.github.com>
2020-04-21 12:59:23 -0500

    Edit to check for new Reddit links

--- RedditSuicideWatchWebsite.js
+++ RedditSuicideWatchWebsite.js
@@ -4,10 +4,21 @@ function correctInput() {
 	input = document.getElementById("link").value;
 	var firstR = input.indexOf("r");
 	var secondR = input.indexOf("r", firstR + 1); 
+
+	//If new reddit link, convert to old
+	var checkNewRedditCom = input.substring(firstR, secondR + 1);
+	if (checkNewRedditCom == "reddit.com/r"){
+		input = "old." + input
+	}
+
 	var checkOldRedditCom = input.substring(input.indexOf("o"), secondR + 1);
 	//Checks whether correct link was given.
+	
+
+
 	if (checkOldRedditCom == "old.reddit.com/r") {
-		//document.getElementById("submitButton").href = "results.html";
+		document.getElementById("submitButton").href = "results.html";
+		test();
 	} else {
 		invalidInput();
 		document.getElementById("submitButton").href = "index.html";
@@ -17,6 +28,7 @@ function correctInput() {
 function invalidInput() {
 	//In the future we can try to make it show alert text so it doesn't need to
 	//redirect to a new link.
+	alert("Invalid Input!");
 }
 
 function submitKeyClicked(keyPressed) {
@@ -24,4 +36,9 @@ function submitKeyClicked(keyPressed) {
 	if (keyPressed.keyCode === 13) {
 		document.getElementById("submitButton").click();
 	}
+}
+
+function test() {
+	//Can remove later, meant to check all the logic was right
+	alert(input + " It worked!");
 }
